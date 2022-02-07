b465263f9ba66a0f5edcd0f7fdfabf3102ea9a50
unknown <kenblackuoo@gmail.com>
2022-02-07 15:37:46 -0500

    Created the shell of our Class files into our project with several pass methods

---
+++ game.py
@@ -0,0 +1,26 @@
+#(10 points): As a player, I want the correct player to win a given round based on the choices* made by each player.  
+#(10 points): As a player, I want the game of RPSLS to be at minimum a ‘best of three’ to decide a winner. 
+from human import Human
+from ai import AI
+
+class Game:
+    def __init__(self):
+        self.player1 = Human()
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        print('\nWelcome to Rock, Paper, Scissors, Lizard, Spock (RPSLS).')
+        pass
+
+    def game_type(self):
+        pass
+
+    def display_winner(self):
+        pass
+
+    def tbd(self):
+        pass
+
+
---
+++ player.py
@@ -0,0 +1,15 @@
+# (10 points): As a developer, I want to store all of the gesture options/choices in a list. I want to find
+# a way to utilize the list of gestures within my code (display gesture options, assign player a gesture, etc). 
+    #the gesture list is in line 8
+
+#This is a Parent Class to support different types of players
+class Player:
+     
+    def __init__(self):
+        self.gesture_list = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+        self.wins = 0
+        self.name = ''
+        self.gesture_selected = ''
+        
+    def choose_gesture(self):
+        pass #method overide will be used by child classes
