5d92297e1dc54bbffd6245caf401cfb396c69b46
unknown <kenblackuoo@gmail.com>
2022-02-08 07:29:18 -0500

    Updated the child classes (AI and Human) to properly display each method's value(s) within the game.py file

--- ai.py
+++ ai.py
@@ -1,13 +1,19 @@
-# (15 points): As a developer, I want to find a way to properly incorporate inheritance into my game.
-# This is the Child Class to the Player_Mode Parent which establishes inheritance
+#(15 points): As a developer, I want to find a way to properly incorporate inheritance into my game. 
+#This is the Child Class to the Player Parent Class which establishes inheritance
 
 from player import Player
 import random
 
-
 class AI(Player):
     def __init__(self):
         super().__init__()
+        self.set_name()
+        self.choose_gesture()
 
+    def set_name(self):
+        self.name = 'SkyNet'
+    
     def choose_gesture(self):
-        self.gestured_selected = random.choice(self.gestures)
+        selected_gesture = random.choice(self.gesture_list)
+        self.gesture_selected = selected_gesture
+
--- human.py
+++ human.py
@@ -1,5 +1,5 @@
 #(15 points): As a developer, I want to find a way to properly incorporate inheritance into my game. 
-#This is the Child Class to the Player_Mode Parent which establishes inheritance
+#This is the Child Class to the Player Parent Class which establishes inheritance
 
 from player import Player
 
@@ -7,10 +7,15 @@ class Human(Player):
     def __init__(self):
         super().__init__()
 
+    def set_name(self):
+        self.name = input("Please enter in a player name: ")
+
     def choose_gesture (self):
-        selected_gesture = input(f'Enter in the gesture you want to use: "{self.gesture_list[0]}", "{self.gesture_list[1]}", "{self.gesture_list[2]}", "{self.gesture_list[3]}", "{self.gesture_list[4]}" : ')
+        selected_gesture = input(f'Enter in the gesture you want to use: "{self.gesture_list[0]}", "{self.gesture_list[1]}", "{self.gesture_list[2]}", "{self.gesture_list[3]}", "{self.gesture_list[4]}": ')
         if selected_gesture in self.gesture_list:
             self.gesture_selected = selected_gesture
+            print('Gesture recorded.')
         else:
-            print('Please make sure to type "Rock" for Rock or "Lizard" for Lizard, as examples.')
+            print('Invalid Entry.')
+            print('Please make sure to type "Rock" for Rock or "Lizard" for Lizard, as examples (case sensitive).')
             self.choose_gesture()
