27273ee84201a8d1684a66000489f6b86cc4a326
unknown <kenblackuoo@gmail.com>
2022-02-08 11:35:29 -0500

    Updated the human.py class with logic to have the user input numbers for gesture selection vs. entering a string of text

--- human.py
+++ human.py
@@ -1,5 +1,7 @@
 #(15 points): As a developer, I want to find a way to properly incorporate inheritance into my game. 
-#This is the Child Class to the Player Parent Class which establishes inheritance
+    #This is the Child Class to the Player Parent Class which establishes inheritance
+#(5 points): As a developer, I want to account for and handle bad user input, ensuring that any user input is validated and reobtained if necessary.
+    #lines 25 and 46 for input validation (if/else conditional)
 
 from player import Player
 
@@ -11,11 +13,37 @@ class Human(Player):
         self.name = input("Please enter in a player name: ")
 
     def choose_gesture (self):
-        selected_gesture = input(f'Enter in the gesture you want to use: "{self.gesture_list[0]}", "{self.gesture_list[1]}", "{self.gesture_list[2]}", "{self.gesture_list[3]}", "{self.gesture_list[4]}": ')
-        if selected_gesture in self.gesture_list:
-            self.gesture_selected = selected_gesture
-            print('Gesture recorded.')
-        else:
-            print('Invalid Entry.')
-            print('Please make sure to type "Rock" for Rock or "Lizard" for Lizard, as examples (case sensitive).')
-            self.choose_gesture()
+        gestured_confirmed = False
+        print(f'Enter in "0" for {self.gesture_list[0]}')
+        print(f'Enter in "1" for {self.gesture_list[1]}')
+        print(f'Enter in "2" for {self.gesture_list[2]}')
+        print(f'Enter in "3" for {self.gesture_list[3]}')
+        print(f'Enter in "4" for {self.gesture_list[4]}')
+        
+        while gestured_confirmed is False:
+            selected_gesture = int(input('Enter in the number (gesture) you want to use: '))
+            if selected_gesture >= 0 and selected_gesture <= 4:
+                if selected_gesture == 0:
+                    self.gesture_selected = self.gesture_list[0]
+                    print('Gesture recorded.')
+                    gestured_confirmed = True
+                elif selected_gesture == 1:
+                    self.gesture_selected = self.gesture_list[1]
+                    print('Gesture recorded.')
+                    gestured_confirmed = True
+                elif selected_gesture == 2:
+                    self.gesture_selected = self.gesture_list[2]
+                    print('Gesture recorded.')
+                    gestured_confirmed = True
+                elif selected_gesture == 3:
+                    self.gesture_selected = self.gesture_list[3]
+                    print('Gesture recorded.')
+                    gestured_confirmed = True
+                elif selected_gesture == 4:
+                    self.gesture_selected = self.gesture_list[4]
+                    print('Gesture recorded.')
+                    gestured_confirmed = True
+            else:
+                print('Invalid Entry.')
+                print('Please make sure to type a number between "0" and "4" per instructions above.')
+                
